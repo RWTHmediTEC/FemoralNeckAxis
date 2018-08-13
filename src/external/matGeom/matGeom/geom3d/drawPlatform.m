e80db37ec991b8e513d5c015739fa006fa9dd1cf
MCMF <>
2018-08-13 10:00:09 +0200

    submodule update

--- src/external/matGeom/matGeom/geom3d/drawPlatform.m
+++ src/external/matGeom/matGeom/geom3d/drawPlatform.m
@@ -1,4 +1,35 @@
 function  varargout = drawPlatform(plane, siz, varargin)
+%DRAWPLPLATFORM Draw a rectangular platform with a given size
+%
+%   drawPlatform(PLANE, SIZ) draws a rectangular platform with the
+%   dimensions specified by SIZ. If SIZ contains only one value instaed of 
+%   two the platform will be quadratic.
+%
+%   drawPlatform(...,'PropertyName',PropertyValue,...) sets the value of 
+%   the specified patch property. Multiple property values can be set with
+%   a single statement. See function patch for details.
+%
+%   drawPlane3d(AX,...) plots into AX instead of GCA.
+%
+%   H = drawPlatform(...) returns a handle H to the patch object.
+%
+%   Example
+%
+%     p0 = [1 2 3];
+%     v1 = [1 0 1];
+%     v2 = [0 -1 1];
+%     plane = [p0 v1 v2];
+%     axis([-10 10 -10 10 -10 10]);
+%     drawPlatform(plane, [7,3])
+%     set(gcf, 'renderer', 'zbuffer');
+%
+%   See also
+%   planes3d, createPlane, patch
+
+% ------
+% Author: oqilipo
+% Created: 2018-08-09
+% Copyright 2018
 
 %% Parse inputs
 if numel(plane) == 1 && ishandle(plane)
@@ -12,7 +43,9 @@ end
 
 p=inputParser;
 addRequired(p,'plane',@(x) size(x,1)==1 && isPlane(x))
-parse(p,plane)
+addRequired(p,'siz',@(x)validateattributes(x,{'numeric'},...
+    {'size',[1, nan],'positive','nonnan','real','finite'}))
+parse(p,plane, siz)
 
 if ~isempty(varargin)
     if length(varargin) == 1
@@ -30,6 +63,10 @@ if ~isempty(varargin)
 else
     % default face color
     varargin = {'FaceColor', 'm'};
+end
+
+if numel(siz)==1
+    siz(2)=siz(1);
 end
 
 
