f56fd06a844c1a1b35f57cae5a41396bfe5c2806
MCMF <>
2020-08-13 11:05:05 +0200

    fixed camera view

--- src/ANA_LoadSubject.m
+++ src/ANA_LoadSubject.m
@@ -18,37 +18,40 @@ end
 
 %% Check direction of neck and shaft axis
 % Neck axis should point in lateral direction
-[~, NeckShaftIts, ShaftNeckIts] = distanceLines3d(GD.Subject.NeckAxis, GD.Subject.ShaftAxis);
+[~, NeckShaftIts] = distanceLines3d(GD.Subject.NeckAxis, GD.Subject.ShaftAxis);
 if sign(GD.Subject.NeckAxis(4:6)) ~= sign(normalizeVector3d(NeckShaftIts(1:3)-GD.Subject.NeckAxis(1:3)))
     GD.Subject.NeckAxis(4:6)=-GD.Subject.NeckAxis(4:6);
 end
-% Shaft axis should point in distal direction
-if sign(GD.Subject.ShaftAxis(4:6)) ~= sign(normalizeVector3d(GD.Subject.ShaftAxis(1:3)-ShaftNeckIts))
-    GD.Subject.ShaftAxis(4:6)=-GD.Subject.ShaftAxis(4:6);
-end
-% !!! Include sanity checks after transformation !!!
-
 GD.Subject.initalNeckAxis = normalizeLine3d(GD.Subject.NeckAxis);
-GD.Subject.ViewVector(1,:)= normalizeVector3d(GD.Subject.ShaftAxis(4:6));
-switch GD.Subject.Side
-    case 'R'
-        GD.Subject.ViewVector(2,:)= normalizeVector3d(...
-            crossProduct3d(GD.Subject.ViewVector(1,:),GD.Subject.initalNeckAxis(4:6)));
-    case 'L'
-        GD.Subject.ViewVector(2,:)= normalizeVector3d(...
-            crossProduct3d(GD.Subject.initalNeckAxis(4:6), GD.Subject.ViewVector(1,:)));
-end
+
 
 %% Create initial transformation
 TRANS = createTranslation3d(-GD.Subject.initalNeckAxis(1:3));
 ROT = createRotationVector3d(GD.Subject.initalNeckAxis(4:6),[0 0 1]);
 GD.Subject.TFM = ROT*TRANS;
 
-GD.Subject.ViewVector = transformVector3d(GD.Subject.ViewVector, GD.Subject.TFM);
+
 
 if GD.Visualization == 1
     %% Configure subplots
-    switch GD.Subject.Side; case 'R'; Side = 'Right'; case 'L'; Side = 'Left'; end
+    GD.Subject.ViewVector(1,:)= normalizeVector3d(GD.Subject.ShaftAxis(4:6));
+    GD.Subject.ViewVector(2,:)= normalizeVector3d(crossProduct3d(GD.Subject.ViewVector(1,:),GD.Subject.initalNeckAxis(4:6)));
+    GD.Subject.ViewVector = transformVector3d(GD.Subject.ViewVector, GD.Subject.TFM);
+    if GD.Subject.ViewVector(1,3) > 0
+        GD.Subject.ViewVector(1,:)=-GD.Subject.ViewVector(1,:);
+    end
+    switch GD.Subject.Side
+        case 'R'
+            Side = 'Right';
+            if GD.Subject.ViewVector(2,2) < 0
+                GD.Subject.ViewVector(2,:)=-GD.Subject.ViewVector(2,:);
+            end
+        case 'L'
+            Side = 'Left';
+            if GD.Subject.ViewVector(2,2) < 0
+                GD.Subject.ViewVector(2,:)=-GD.Subject.ViewVector(2,:);
+            end
+    end
     set(GD.Figure.Handle, 'Name', [Side ' femur of subject: ' GD.Subject.Name]);
     % Clear right subplot
     rSP = GD.Figure.RightSpHandle;
@@ -61,8 +64,8 @@ if GD.Visualization == 1
     % Left subject subplot and properties
     lSP = GD.Figure.LeftSpHandle;
     cla(lSP,'reset');
+    xlabel(lSP,'X [mm]'); ylabel(lSP,'Y [mm]'); zlabel(lSP,'Z [mm]');
     axis(lSP,'off');
-%     xlabel(lSP,'X [mm]'); ylabel(lSP,'Y [mm]'); zlabel(lSP,'Z [mm]');
     set(lSP,'Color',GD.Figure.Color);
     light1 = light(lSP); light(lSP, 'Position', -1*(get(light1,'Position')));
     cameratoolbar('SetCoordSys','none')
