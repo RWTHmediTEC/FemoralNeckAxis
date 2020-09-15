2f698347d4f6f7b3ed304ea71bbf3fdf4e485dc2
MCMF <>
2020-09-15 18:17:53 +0200

    example simplified

--- femoralNeckAxis_example.m
+++ femoralNeckAxis_example.m
@@ -1,7 +1,7 @@
 clearvars; close all; opengl hardware
 
 %% Select subject
-Idx = 2;
+Idx = 5;
 
 load('data\F','F')
 
@@ -12,31 +12,21 @@ femur = F(Idx).mesh;
 Side = F(Idx).side;
 
 % Create the neck axis from the vertex indices
-NeckAxis=createLine3d(...
-    femur.vertices(F(Idx).LM.NeckAxis(1),:),...
-    femur.vertices(F(Idx).LM.NeckAxis(2),:));
+NeckAxis = F(Idx).LM.NeckAxis;
 % Create the shaft axis from the vertex indices
-ShaftAxis=createLine3d(...
-    femur.vertices(F(Idx).LM.ShaftAxis(1),:),...
-    femur.vertices(F(Idx).LM.ShaftAxis(2),:));
-% Create the neck orthogonal from the vertex indices
-NeckOrthogonal=createLine3d(...
-    femur.vertices(F(Idx).LM.NeckOrthogonal(1),:),...
-    femur.vertices(F(Idx).LM.NeckOrthogonal(2),:));
-% Neck axis starts at the intersection of neck axis and neck orthogonal
-[~, NeckAxis(1:3), ~] = distanceLines3d(NeckAxis, NeckOrthogonal);
+ShaftAxis = F(Idx).LM.ShaftAxis;
 
 %% Select different options by commenting 
 % Default mode
-[FNAxis, FNATFM] = femoralNeckAxis(femur, Side, NeckAxis, ShaftAxis, ...
+[FNAxis, FNA_TFM] = femoralNeckAxis(femur, Side, NeckAxis, ShaftAxis, ...
     'Subject', num2str(Idx));
 % Silent mode
-% [FNAxis, FNATFM] = femoralNeckAxis(femur, Side, NeckAxis, ShaftAxis, ...
+% [FNAxis, FNA_TFM] = femoralNeckAxis(femur, Side, NeckAxis, ShaftAxis, ...
 %     'Subject', num2str(Idx), 'Visu', false, 'Verbose', false);
 % Other options
-% [FNAxis, FNATFM] = femoralNeckAxis(femur, Side, NeckAxis, ShaftAxis, ...
+% [FNAxis, FNA_TFM] = femoralNeckAxis(femur, Side, NeckAxis, ShaftAxis, ...
 %     'Subject', num2str(Idx), 'Objective', 'dispersion');
-% [FNAxis, FNATFM] = femoralNeckAxis(femur, Side, NeckAxis, ShaftAxis, ...
+% [FNAxis, FNA_TFM] = femoralNeckAxis(femur, Side, NeckAxis, ShaftAxis, ...
 %     'PlaneVariationRange', 12, 'StepSize', 3);
 
 %% Visualization
